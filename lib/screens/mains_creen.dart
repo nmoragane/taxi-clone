cdf6fd4c3ad121d65731934465dc3ef4febeec0a
nmoragane <nmoragane@gmail.com>
2021-06-15 23:19:07 +0530

    setup popup below maps

--- lib/screens/mains_creen.dart
+++ lib/screens/mains_creen.dart
@@ -35,6 +35,142 @@ class _MainScreenState extends State<MainScreen> {
               _controllerGoogleMap.complete(controller);
               newGoogleMapController = controller;
             },
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 320.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15.0),
+                    topLeft: Radius.circular(18.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 16.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    )
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                  vertical: 18.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Text(
+                      "Hi there, ",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                      ),
+                    ),
+                    Text(
+                      "Where to? ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 16.0,
+                              spreadRadius: 0.5,
+                              offset: Offset(0.7, 0.7),
+                            )
+                          ]),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(
+                            "Search Drop off Location",
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Home"),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Your living home address",
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                                fontSize: 12.0,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.work,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Work"),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Your work address",
+                              style: TextStyle(
+                                color: Colors.grey[500],
+                                fontSize: 12.0,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
---
+++ lib/widgets/Divider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Divider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Divider();
+  }
+}
